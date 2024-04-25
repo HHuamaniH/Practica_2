79da46e6e8ec440a9a9b5adef6835913d6833cf0
Carlos Leopoldo Huaroc Carrión <chuaroc@osinfor.gob.pe>
2024-04-25 12:22:10 -0500

    Ajuste código

--- SIGOFCv3/Areas/THabilitante/js/ManPlanManejoForestal/AddEdit.js
+++ SIGOFCv3/Areas/THabilitante/js/ManPlanManejoForestal/AddEdit.js
@@ -817,7 +817,7 @@ ManPM.iniciarTablas = function () {
                         var cell = '<input type="hidden" class="hdDescBloque" value="' + row.descB + '" />' + row.descB;
                         return cell;
                     }
-                },
+                }
             ]
     });
     var contCoordenadas = 1;
